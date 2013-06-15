16ae44eb77767fd72f83fece11cc64adb7c0aaed
Aliaksei Kulbei <a.kulbei@gmail.com>
2013-06-15 16:50:13 +0200

    rails new project skelethon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobboard::Application.config.secret_token = '70df5114bd373b61597aaa6718fdeb6089a61261f5a17beb0ed1bb5e7f4653f4c8e8905bd30644df8a11397e42882e344bc3e1f6ebbcbac8b992e9981e3b2c9a'
